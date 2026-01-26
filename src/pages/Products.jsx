4a11656c10f3404bcba6023057f467ee24f9f29d
PythonDeveloper <pooja.tarar@gmail.com>
2026-01-26 11:49:43 +0530

    aaded

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -15,6 +15,19 @@ function Products(){
                 <img src={product.thumbnail} className="img" />
                 <h2 className="name">{product.title}</h2>
                 <p className="price">${product.price}</p>
+                <select className="quantity">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+                <button className="cart-butt">Add to cart</button>
               </div>
           ))}
         </div>
