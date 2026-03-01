5b04065cc07f40b5241766a0ae393c9ba09739b0
PythonDeveloper <pooja.tarar@gmail.com>
2026-03-01 12:21:12 +0530

    added

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,12 +1,14 @@
 import './orders.css'
+import {useContext} from 'react'
 import Navbar from '../Navbar.jsx'
 import Products from './Products.jsx'
+import { inputvalue } from '../Context.jsx'
 function Orders(){
+        const {inpval,setInpval}=useContext(inputvalue)
         return(
             <>
                 <Navbar/>
-                <h1 className="orders-label">Your Orders</h1>
-            
+                {inpval==='' ? <h1 className='orders-label'>Your Orders</h1> : <Products/>}
             </>
         )
 }
